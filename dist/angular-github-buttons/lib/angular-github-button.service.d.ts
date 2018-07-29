5546c0a8d6e6dcaae9a384d94e99f14500b571b4
Matt Martz <matt.martz@gmail.com>
2018-07-29 12:40:01 -0400

    remove dist

--- dist/angular-github-buttons/lib/angular-github-button.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { HttpClient } from '@angular/common/http';
-export declare class AngularGithubButtonService {
-    private http;
-    private apiUrl;
-    private users;
-    constructor(http: HttpClient);
-    getUserInfo(username: string): any;
-    getRepoInfo(username: string, reponame: string): any;
-}
